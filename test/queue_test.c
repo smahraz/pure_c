8a5d88cc40aadfe8805a41424cc3fed55395b667
smahraz <smahraz@student.1337.ma>
2026-05-13 11:37:43 +0100

    test: Add simple test for `Queue` data structure

---
+++ test/queue_test.c
@@ -0,0 +1,32 @@
+#include "queue.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+
+
+
+bool test_len_and_free_func()
+{
+  Queue* q = queue_init();
+  if (q == NULL)
+    return true;
+  for (int i=0; i < 10; i++){
+    queue_put(q, malloc(sizeof(int)));
+  }
+
+  if (queue_len(q) != 10)
+    return true;
+  
+  queue_free(q, &free);
+
+  return false;
+}
+
+
+
+int main()
+{
+  if (test_len_and_free_func())
+    return 1;
+}
---
+++ test/test.sh
@@ -0,0 +1,33 @@
+
+compile_and_test () {
+  echo At $1:
+  cc -I../include -L.. $1.c -o $1 -lpure_c || exit
+
+  ./$1 || echo "wrong exit code"
+
+  valgrind ./$1 2>/dev/null 1>/dev/null || echo "leak"
+}
+
+
+
+tests=(
+  queue_test
+)
+
+
+TEST_WORKING_DIR=$(pwd)
+LIB_NAME="libpure_c.a"
+
+
+cd ..
+make re
+cd $TEST_WORKING_DIR
+
+for test in "${tests[@]}";
+do
+  compile_and_test $test
+  rm -fv $test
+done
+
+
+
